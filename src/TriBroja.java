fe0e99c891ce572bd0be0fa61ae0e36851a0075f
GacovAndre <gacovandre79@gmail.com>
2023-12-05 12:42:17 +0100

    first commit

---
+++ src/TriBroja.java
@@ -0,0 +1,13 @@
+public class TriBroja {
+public static int findMin(int n1, int n2) {
+int min;
+if (n1<n2)
+min = n1;
+else
+min = n2;
+return min;
+}
+public static void main(String[] args) {
+System.out.println("Minimum od dvata broja e:" + findMin(10, 5));
+}
+}
